14e126a8346fd2fed90d67aab2e459c84c043839
vinayak-ranbhare <121924434+vinayak-ranbhare@users.noreply.github.com>
2024-03-06 17:07:22 +0530

    Add files via upload

---
+++ src/test/java/testng/BasicTestNGExample.java
@@ -0,0 +1,31 @@
+package testng;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicTestNGExample {
+    @Test
+    public void testCase1() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.makemytrip.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+    @Test
+    public void testCase2() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.cricbuzz.com/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+    @Test
+    public void testCase3() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.close();
+    }
+}
